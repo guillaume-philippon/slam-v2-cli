5f87677aa50618e592e505e7dd870f8bcc46d055
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2019-12-28 22:02:22 +0100

    Add some comments

--- domains/arguments.py
+++ domains/arguments.py
@@ -9,25 +9,57 @@ def domains_argparse(parser):
 
     :param parser: main argument parser
     """
+    # First of all, we store action value
     subparsers = parser.add_subparsers(help='Action', dest='action')
+
+    # All action value are listed here
+    # - list: list all item in domains
+    # - create: create a new domain
+    # - update: modify a existing domain. All value are not mutable
+    # - delete: destroy a domain and all its entries
+    # - show: show detail of a specific domain
+    # - add: add a new entry on a domain
+    # - remove: remove a specific entry in a domain.
     subparsers.add_parser('list', help='list all domains')
-    show = subparsers.add_parser('show', help='show detail of a domain')
     create = subparsers.add_parser('create', help='create new domain')
-    add = subparsers.add_parser('add', help='Add new fqdn in a domain')
-    update = subparsers.add_parser('update', help='Update domain information')
+    update = subparsers.add_parser('update', help='update domain informations')
+    delete = subparsers.add_parser('delete', help='delete a domain and all associated entry')
+    show = subparsers.add_parser('show', help='show detail of a specific domain')
+    add = subparsers.add_parser('add', help='Add new entry in a domain')
+    remove = subparsers.add_parser('remove', help='remove a entry in a domain')
 
-    show.add_argument('domain', help='domain you want to show')
-
+    # To create a domain, we need a domain name and optionaly
+    # - description: a description of the domain
+    # - contact: a contact email for the domain
+    # - dns-master: the DNS master of the domain
     create.add_argument('domain', help='domain name')
-    create.add_argument('--description', help='description of item')
-    create.add_argument('--contact', help='contact')
+    create.add_argument('--description', help='a description of the domain')
+    create.add_argument('--contact', help='a contact email for the domain')
     create.add_argument('--dns-master', help='DNS master address')
 
-    add.add_argument('fqdn', help='Create a new DNS entry')
-    add.add_argument('--description', help='description of item')
-    add.add_argument('--type', help='DNS type')
+    # To delete a domain, we just need to know the domain we want to delete
+    delete.add_argument('domain', help='domain name')
 
+    # To update a domain, we need to know the domain name and the following value are
+    # mutable
+    # - description: a description of the domain
+    # - contact: a contact email for the domain
+    # - dns-master: the DNS master of the domain
     update.add_argument('domain', help='domain name')
-    update.add_argument('--description', help='description of item')
-    update.add_argument('--contact', help='contact')
+    update.add_argument('--description', help='description of the domain')
+    update.add_argument('--contact', help='contact email for the domain')
     update.add_argument('--dns-master', help='DNS master address')
+
+    # To have detail of a specific domain, we just need to know the name of it
+    show.add_argument('domain', help='domain name')
+
+    # To add a new entry of a domain, we need know, the Full Qualified Domain Name (fqdn)
+    # of the entry and optionaly
+    # - description: a description of the entry
+    # - type: the type of entry. The type is a bind type (A, AAAA, CNAME, ...)
+    add.add_argument('fqdn', help='Full Qualified Domain Name')
+    add.add_argument('--description', help='description of the entry')
+    add.add_argument('--type', help='bind type')
+
+    # To remove a specific entry, we need to know which fqdn remove
+    remove.add_argument('fqdn', help='Full Qualified Domain Name')
--- domains/controller.py
+++ domains/controller.py
@@ -34,31 +34,6 @@ class SlamDomainController:
             print('Domain {} creation failed with status {}'.format(result['domain'],
                                                                     result['status']))
 
-    def add(self, options):
-        """
-        Add a new entry into a domain. A entry is a fqdn (ie www.example.com)
-
-        :param self: object itself
-        :param options: arguments pass throught CLI
-        """
-        fqdn = options.fqdn.split('.', 1)
-        name = fqdn[0]
-        domain = fqdn[1]
-        entry = dict()
-        if options.description is not None:
-            entry['description'] = options.description
-        if options.type is not None:
-            entry['type'] = options.type
-        result = self.api.create('domains', domain, entry, field=name)
-        if result['status'] == 'done':
-            print('Name resolution {} as been added to domain {}'.format(name, domain))
-        else:
-            print('Name resolution {}.{} addition has failed with status {}'.format(
-                result['entry'],
-                result['domain'],
-                result['status']
-            ))
-
     def update(self, options):
         """
         Modify a domain. This allow to change contact email, description and dns-master
@@ -79,3 +54,58 @@ class SlamDomainController:
         else:
             print('Domain {} modification failed with status {}'.format(options.domain,
                                                                         result['status']))
+
+    def delete(self, options):
+        """
+        Delete a domain on SLAM.
+
+        :param options: arguments pass throught CLI
+        :return:
+        """
+        result = self.api.delete('domains', options.domain)
+        if result['status'] == 'done':
+            print('Domain {} has been deleted'.format(options.domain))
+        else:
+            print('domain {} deletation has failed with status {}'.format(options.domain,
+                                                                       result['status']))
+
+    def add(self, options):
+        """
+        Add a new entry into a domain. A entry is a fqdn (ie www.example.com)
+
+        :param self: object itself
+        :param options: arguments pass throught CLI
+        """
+        fqdn = options.fqdn.split('.', 1)
+        ns = fqdn[0]
+        domain = fqdn[1]
+        entry = dict()
+        if options.description is not None:
+            entry['description'] = options.description
+        if options.type is not None:
+            entry['type'] = options.type
+        result = self.api.create('domains', domain, entry, field=ns)
+        if result['status'] == 'done':
+            print('Name resolution {} as been added to domain {}'.format(ns, domain))
+        else:
+            print('Name resolution {}.{} addition has failed with status {}'.format(
+                result['entry'],
+                result['domain'],
+                result['status']
+            ))
+
+    def remove(self, options):
+        """
+        Remove a specific entry into a domain. The entry is represented by it s fqdn
+
+        :param options: argument pass throught CLI
+        :return:
+        """
+        fqdn = options.fqdn.split('.', 1)
+        ns = fqdn[0]
+        domain = fqdn[1]
+        result = self.api.delete('domains', domain, field=ns)
+        if result['status'] == 'done':
+            print('Name Resolution {} as been removed from domain {}'.format(ns, domain))
+        else:
+            print('Oops')
